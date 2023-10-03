9b2a5ed29fa7386621b76450ca2a81bf0b1f868b
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-10-03 23:57:21 +0530

    day 1 completed

---
+++ Day 1/Day1.java
@@ -0,0 +1,9 @@
+//package 100_DAYS_CODING_CHALLENGE;
+import java.util.*;
+public class Day1{
+    public static void main(String[] args){
+        System.out.println("welcome to day 1");
+    }
+
+
+}
