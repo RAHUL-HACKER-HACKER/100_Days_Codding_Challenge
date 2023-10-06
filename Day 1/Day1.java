8a7f5c4af27b1f8bedc8d50e181bfde90ff9e06a
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-10-06 13:45:42 +0530

    day 4 completed

--- Day 1/Day1.java
+++ Day 1/Day1.java
@@ -1,9 +1,40 @@
-//package 100_DAYS_CODING_CHALLENGE;
-import java.util.*;
-public class Day1{
-    public static void main(String[] args){
-        System.out.println("welcome to day 1");
+import java.util.Stack;
+
+public class Day1 {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int operand2 = stack.pop();
+                int operand1 = stack.pop();
+                int result = performOperation(operand1, operand2, token);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        
+        return stack.pop();
     }
-
-
+    
+    private boolean isOperator(String token) {
+        return "+".equals(token) || "-".equals(token) || "*".equals(token) || "/".equals(token);
+    }
+    
+    private int performOperation(int operand1, int operand2, String operator) {
+        switch (operator) {
+            case "+":
+                return operand1 + operand2;
+            case "-":
+                return operand1 - operand2;
+            case "*":
+                return operand1 * operand2;
+            case "/":
+                return operand1 / operand2;
+            default:
+                return 0;
+        }
+    }
+    
 }
