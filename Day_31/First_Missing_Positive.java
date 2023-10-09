05496d6895b8f34e6cb0459295e16fe13dea01c9
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-10-09 23:41:15 +0530

    day 31 comppeted

---
+++ Day_31/First_Missing_Positive.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+    
+        //the first two conditions in the if statement take care 
+        //that negatives and elements greater than the size of 
+        //the array are ignored, while applying cycle sort, and 
+        //rest is the similar code of cycle sort
+
+        int i = 0;
+        while(i < nums.length) {
+            int correct = nums[i] - 1;
+            if(nums[i] <= nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+
+        int count = 0;
+
+        for(int j = 0; j < nums.length; j++) {
+
+            if(nums[j] != j + 1) {
+                return j + 1;
+            }
+        }
+
+        return nums.length + 1;
+    }
+}
