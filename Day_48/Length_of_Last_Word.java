19d2ffa70d47ea19b194db3fe0016d498d05ef24
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-10-10 13:53:42 +0530

    day 48 completed

---
+++ Day_48/Length_of_Last_Word.java
@@ -0,0 +1,18 @@
+package Day_48;
+
+public class Length_of_Last_Word {
+    
+}class Solution {
+    public int lengthOfLastWord(String s) {
+        String str = s.trim();
+        int count = 0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i) != ' '){
+                count++;
+            }else{
+                break;
+            }
+        } 
+        return count;
+    }
+}
