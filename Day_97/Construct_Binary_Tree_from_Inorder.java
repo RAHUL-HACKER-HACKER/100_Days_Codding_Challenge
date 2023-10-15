dc7a12017b08d2abd9e67f4e27798328fec2e09d
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2023-10-15 12:34:12 +0530

    day 97 completed

---
+++ Day_97/Construct_Binary_Tree_from_Inorder.java
@@ -0,0 +1,38 @@
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return helper(inorder, postorder, 0, inorder.length - 1, 0, postorder.length - 1, map);
+    }
+
+    private TreeNode helper(int[] inorder, int[] postorder, int inStart, int inEnd, int postStart, int postEnd, Map<Integer, Integer> map) {
+        if(inStart > inEnd || postStart > postEnd) return null;
+
+        TreeNode root = new TreeNode(postorder[postEnd]);
+
+        int inRoot = map.get(root.val);
+
+        root.left = helper(inorder, postorder, inStart, inRoot - 1, postStart, postEnd - (inEnd - inRoot) - 1, map);
+        root.right = helper(inorder, postorder, inRoot + 1, inEnd, postEnd - (inEnd - inRoot), postEnd - 1, map);
+        
+        return root;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
