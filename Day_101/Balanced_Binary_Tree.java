6792a77de1554f469aa8ee2dcb2151f86e4c72e0
RAHUL-HACKER-HACKER <rahulk841501@gmail.com>
2024-01-04 14:07:25 +0530

    add

--- Day_101/Balanced_Binary_Tree.java
+++ Day_101/Balanced_Binary_Tree.java
@@ -7,7 +7,7 @@ class Solution {
 	public int Height(TreeNode root) {
 
 		if (root == null)  return 0;
-		
+		int leftHeight = Height(root.left);
 		int rightHight = Height(root.right);
 		if (leftHeight == -1 || rightHight == -1)  return -1;
         if (Math.abs(leftHeight - rightHight) > 1)  return -1;
